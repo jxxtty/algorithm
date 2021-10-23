9d58988f0c4b331d3c3a4b47d42d58b161fc2fc9
JaeheeAhn <jaehee941106@gmail.com>
2021-10-23 21:49:19 +0900

    programmers_level2_Test10

---
+++ src/programmers_level2/Test10.java
@@ -0,0 +1,37 @@
+package programmers_level2;
+// ¿¬½À¹®Á¦_¶¥µû¸Ô±â
+import java.util.Arrays;
+
+public class Test10 {
+	int solution(int[][] land) {
+        int answer = 0;
+        for(int i = 1 ; i < land.length ; i++){
+            land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+            land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
+            land[i][2] += Math.max(Math.max(land[i-1][0], land[i-1][1]), land[i-1][3]);
+            land[i][3] += Math.max(Math.max(land[i-1][0], land[i-1][1]), land[i-1][2]);
+        }
+        
+        answer = Arrays.stream(land[land.length-1]).max().getAsInt();
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		Test10 t = new Test10();
+		
+		int[][] land = {
+				{1, 2, 3, 5},
+				{5, 6, 7, 8},
+				{4, 3, 2, 1}
+		};
+		System.out.println(t.solution(land)); // 16 (5 + 7 + 4)
+		
+		int[][] land2 = {
+				{1, 2, 3, 5},
+				{5, 6, 7, 100},
+				{4, 3, 2, 1}
+		};
+		System.out.println(t.solution(land2)); // 107 (3 + 100 + 4)
+	}
+
+}
