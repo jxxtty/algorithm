4cb1e4f03aef083a9f4a4c85f9567721c23bdd2b
JaeheeAhn <jaehee941106@gmail.com>
2021-10-31 20:16:25 +0900

    programmers_level2_Test15

---
+++ src/programmers_level2/Test15.java
@@ -0,0 +1,32 @@
+package programmers_level2;
+
+import java.util.Arrays;
+
+// ¿¬½À_Å½¿å¹ý_±¸¸íº¸Æ®
+public class Test15 {
+	public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        int min = 0;
+        for(int max = people.length-1; min <= max ; max-- ){
+            if(people[min] + people[max] <= limit) min++;
+            answer++;
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		Test15 t = new Test15();
+		
+		int[] people = {70, 50, 80, 50};
+		int limit = 100;
+		System.out.println(t.solution(people, limit)); // 3
+		
+		int[] people2 = {70, 80, 50};
+		int limit2 = 100;
+		System.out.println(t.solution(people2, limit2)); // 3
+	}
+
+}
